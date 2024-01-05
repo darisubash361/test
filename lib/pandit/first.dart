67b27016f82b88663ebf700bc4da9731dc7c074d
darisubash361 <monkey.temple361@gmail.com>
2024-01-05 08:57:07 +0545

    Guru

---
+++ lib/pandit/first.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Guru extends StatelessWidget {
+  const Guru({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pandit is a laad'),
+      ),
+    );
+  }
+}
